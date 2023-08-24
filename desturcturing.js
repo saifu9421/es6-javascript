f4e9196db82a9a4c7f85431c8f1de9704fbb5e59
saifu9421 <saifursaif905@gmail.com>
2023-08-25 03:18:50 +0600

    added  a destrueture

--- desturcturing.js
+++ desturcturing.js
@@ -6,7 +6,11 @@ const actor = {
 };
  
 // if right  side is an object left side  of desturcturing will  be  object as well
-const {phone} =  actor;
+
+// use property name  as a variable  that contains the property value 
+
+const {phone,age:boyos} =  actor;
+const {name} =  actor;          
 
 // const phone =  actor.phone;
 // const name = actor.name;
@@ -14,13 +18,31 @@ const {phone} =  actor;
 
 
 console.log(phone);
+console.log(phone);        
 console.log(phone);
 console.log(phone);
 console.log(phone);
-console.log(phone);
-// console.log(age);
-// console.log(age);
-// console.log(age);
-// console.log(age);
+console.log(boyos);
+console.log(boyos);
+console.log(boyos);
+console.log(boyos);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
 
-// console.log(name);
+
+// array  destructuring 
+ const  numbers = [12,1,14,15,16,17];
+
+ const [first, second] = numbers;
+console.log(first);
+console.log(second);
+
+
+// Advanced
+function doubleThem(a ,b){
+    return [a*2 , b*2];
+};
+ const [prothom ,ditio] =  doubleThem(6 , 9);
+ console.log(prothom , ditio);
